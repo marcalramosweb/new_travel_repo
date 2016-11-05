cfc303081ebe85baacb8a12629281a3029f3d44b
Marcal ramos <marcalramos@gmail.com>
2016-11-05 14:21:45 +0000

    creation content layout

--- front-page.php
+++ front-page.php
@@ -4,6 +4,13 @@
     <img class="" src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide_1.jpg" alt="<?php bloginfo('name');?>" />
     <!-- <img class="" src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide_2.jpg" alt="<?php bloginfo('name');?>" />
     <img class="" src="<?php bloginfo('stylesheet_directory');?>/assets/img/slide_3.jpg" alt="<?php bloginfo('name');?>" /> -->
+
+    <!--
+    *****************************************************
+                      Search content begin here...
+    *****************************************************
+    -->
+
       <div class="container main-wrapper-form">
         <div class="container __form-wrapper">
           <div class="serv-pannel">
@@ -108,13 +115,82 @@
 
       </div>
 
+
   </div>
 </div>
 
-    <div class="container">
-      <div class="starter-template">
-        <h1>Bootstrap starter template palavras</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+<!--
+*****************************************************
+                  Search content end here...
+*****************************************************
+-->
+
+<!--
+*****************************************************
+                  Body content goes here...
+*****************************************************
+-->
+
+    <div class="container main-body-container">
+      <div class="starter-template text-center">
+        <h1 class="starter-template__title">Welcome To Aguia Travel</h1>
+        <p class="lead starter-template__subtitle">We are here to help, fell free to search and find wat you are looking for, or try to call us on.</p>
+        <p class="lead starter-template__phone"><i class="fa fa-phone" aria-hidden="true"></i> 000-000-000</p>
       </div>
-    </div><!-- /.container -->
+
+    <div class="divider-container">
+      <div class="clearfix"></div>
+      <h4 class="divider-container__title">Major Destination with 50% off this week</h4>
+       <hr class="divider-container__divider-r">
+    </div>
+
+
+      <div class="main-body-container__inner-container">
+          <div class="col-sm-4 col-md-4 main-body-container__inner-container-rectcard">
+            <div class="main-body-container__inner-container-rectcard-image">
+              <img class="img-responsive" src="<?php bloginfo('stylesheet_directory');?>/assets/img/flight-test.jpg" alt="" />
+              <div class="main-body-container__inner-container-rectcard-content">
+                <h3>Title here</h3>
+                <p>description here.....</p>
+                <div class="main-body-container__inner-container-rectcard-price-tag text-right">
+                  <span class="">From $475.33</span>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
+            </div>
+          </div><!-- / main body inner-container-rectcard -->
+
+          <div class="col-sm-4 col-md-4 main-body-container__inner-container-rectcard">
+            <div class="main-body-container__inner-container-rectcard-image">
+              <img class="img-responsive" src="<?php bloginfo('stylesheet_directory');?>/assets/img/flight_.jpg" alt="" />
+              <div class="main-body-container__inner-container-rectcard-content">
+                <h3>Title here</h3>
+                <p>description here.....</p>
+                <div class="main-body-container__inner-container-rectcard-price-tag text-right">
+                  <span class="">From $584.00</span>
+                  <div class="clearfix"></div>
+                </div>
+              </div>
+            </div>
+          </div><!-- / main body inner-container-rectcard -->
+
+          <div class="col-sm-4 col-md-4 main-body-container__inner-container-rectcard">
+            <div class="main-body-container__inner-container-rectcard-image">
+              <img class="img-responsive" src="<?php bloginfo('stylesheet_directory');?>/assets/img/africa.jpg" alt="" />
+              <div class="main-body-container__inner-container-rectcard-content">
+                <h3>Title here</h3>
+                <p>description here.....</p>
+                <div class="main-body-container__inner-container-rectcard-price-tag text-right">
+                  <span class="">From $980.00</span>
+
+                  <div class="clearfix"></div>
+                </div>
+              </div>
+            </div>
+          </div><!-- / main body inner-container-rectcard -->
+
+            <div class="clearfix"></div>
+      </div><!-- / main body inner-container -->
+
+    </div><!-- /.container / main body container -->
 <?php get_footer();?>
